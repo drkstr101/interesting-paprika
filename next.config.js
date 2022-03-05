3c463237258f3ae88eadcfcc1f58378bcbae2a4e
Aaron Miller <amiller@watheia.io>
2022-03-05 23:53:30 +0000

    run prettier

--- next.config.js
+++ next.config.js
@@ -2,40 +2,40 @@ const { BASE_PATH } = process.env;
 const sourcebit = require('sourcebit');
 const sourcebitConfig = require('./sourcebit.js');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === 'true'
 });
 
 sourcebit.fetch(sourcebitConfig);
 
 module.exports = withBundleAnalyzer({
-    basePath: BASE_PATH,
-    assetPrefix: BASE_PATH,
-    trailingSlash: true,
-    experimental: {
-        // Prefer loading of ES Modules over CommonJS
-        esmExternals: true
-    },
-    // minify output
-    swcMinify: true,
-    devIndicators: {
-        autoPrerender: false
-    },
-    eslint: {
-        // Allow production builds to successfully complete even if your project has ESLint errors.
-        ignoreDuringBuilds: true
-    },
-    webpack: (config, { dev }) => {
-        // Tell webpack to ignore watching content files in the content folder.
-        // Otherwise webpack recompiles the app and refreshes the whole page.
-        // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
-        // function to update the content on the page without refreshing the
-        // whole page
-        config.watchOptions.ignored.push('/content/');
-        if (dev) {
-            // enable tree shaking for development mode, on production it is on by default
-            config.optimization.usedExports = true;
-        }
-
-        return config;
+  basePath: BASE_PATH,
+  assetPrefix: BASE_PATH,
+  trailingSlash: true,
+  experimental: {
+    // Prefer loading of ES Modules over CommonJS
+    esmExternals: true
+  },
+  // minify output
+  swcMinify: true,
+  devIndicators: {
+    autoPrerender: false
+  },
+  eslint: {
+    // Allow production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
+  webpack: (config, { dev }) => {
+    // Tell webpack to ignore watching content files in the content folder.
+    // Otherwise webpack recompiles the app and refreshes the whole page.
+    // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
+    // function to update the content on the page without refreshing the
+    // whole page
+    config.watchOptions.ignored.push('/content/');
+    if (dev) {
+      // enable tree shaking for development mode, on production it is on by default
+      config.optimization.usedExports = true;
     }
+
+    return config;
+  }
 });
