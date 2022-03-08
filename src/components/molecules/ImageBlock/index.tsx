7d5e945c90c8bbf49a3fd55cc4c78cb9a535ea0e
Aaron Miller <amiller@watheia.io>
2022-03-08 05:05:48 +0000

    restore ImageBlock :(

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -1,17 +1,16 @@
 import classNames from 'classnames';
-import NextImage from 'next/image';
 import * as React from 'react';
-import getImageInfo from '../../../utils/get-media-info';
+// import getImageInfo from '../../../utils/get-media-info';
 
-const RESPONSIVE_IMAGES = process.env['NODE_ENV'] === 'production';
+// const RESPONSIVE_IMAGES = process.env['NODE_ENV'] === 'production';
 
 export default function ImageBlock(props) {
   const { url, altText } = props;
   if (!url) {
     return null;
   }
-  const filename = url.split('/').pop();
-  const meta = getImageInfo(filename);
+  // const filename = url.split('/').pop();
+  // const meta = getImageInfo(filename);
   const cssClasses = props.className || null;
   const cssId = props.elementId || null;
   const styles = props.styles?.self || {};
@@ -24,28 +23,28 @@ export default function ImageBlock(props) {
     `${annotationPrefix}.elementId#@id`
   ];
 
-  if (RESPONSIVE_IMAGES && meta) {
-    const maxWidth = props.maxWidth ?? meta.responsiveImage.width;
-    const opacity = imageOpacity * 0.01;
-    return (
-      <div id={cssId} style={{ maxWidth, opacity }}>
-        <NextImage
-          id={cssId}
-          className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', cssClasses)}
-          src={url}
-          data-sb-field-path={annotations.join(' ').trim()}
-          width={meta.responsiveImage.width}
-          height={meta.responsiveImage.height}
-          alt={altText}
-          priority={props.priority}
-        ></NextImage>
-      </div>
-    );
-  }
+  // if (meta) {
+  //   const maxWidth = props.maxWidth ?? meta.responsiveImage.width;
+  //   const opacity = imageOpacity * 0.01;
+  //   return (
+  //     <div style={{ maxWidth, opacity }}>
+  //       <NextImage
+  //         id={cssId}
+  //         className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', cssClasses)}
+  //         src={meta.responsiveImage.src}
+  //         data-sb-field-path={annotations.join(' ').trim()}
+  //         width={meta.responsiveImage.width}
+  //         height={meta.responsiveImage.height}
+  //         alt={altText}
+  //         priority={props.priority}
+  //       ></NextImage>
+  //     </div>
+  //   );
+  // }
 
-  if (RESPONSIVE_IMAGES && !filename.endsWith('.svg')) {
-    console.warn(`${filename} not located in media.json, falling back to native image`);
-  }
+  // if (!filename.endsWith('.svg')) {
+  //   console.warn(`${filename} not located in media.json, falling back to native image`);
+  // }
 
   return (
     // eslint-disable-next-line @next/next/no-img-element
